f40380105514c6b45221bf353c16c2d703911c98
nargok <nargok@gmail.com>
2022-12-18 19:12:05 +0900

    basic layout structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,15 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <h1 className='text-2xl'>Hello, world</h1>
+    <h1 className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
+      <div className="p-2"></div>
+      <div className='border rounded p-8 flex justify-between item-center max-w-2xl'>
+        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200'></div>
+      </div>
+    </h1>
   )
 }
 
