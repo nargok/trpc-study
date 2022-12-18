3bc9adf5dda44d3d582b5355b42387c5ad313a44
nargok <nargok@gmail.com>
2022-12-18 20:07:20 +0900

    add tRPC

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,3 +1,18 @@
-import * as trpc from "@trpc/server";
-
+import { procedure, router } from "@/backend/trpc";
 import { z } from "zod";
+
+export const appRouter = router({
+  hello: procedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      return {
+        greeting: `hello ${input?.text ?? "world"}`,
+      };
+    }),
+});
+
+export type AppRouter = typeof appRouter;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { withTRPC } from "@trpc/next";
+import type { AppRouter } from "@/backend/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+// export default MyApp;
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
+
+    return {
+      url,
+      // links: [
+      //   httpBatchLink({
+      //     /**
+      //      * If you want to use SSR, you need to use the server's full URL
+      //      * @link https://trpc.io/docs/ssr
+      //      **/
+      //     url: `${getBaseUrl()}/api/trpc`,
+      //   }),
+      // ],
+      /**
+       * @link https://tanstack.com/query/v4/docs/reference/QueryClient
+       **/
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   **/
+  ssr: true,
+})(MyApp);
