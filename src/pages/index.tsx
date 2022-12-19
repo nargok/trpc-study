1ec79e0b6f0ec6f48e0169db9501610ba455ad0b
nargok <nargok@gmail.com>
2022-12-19 20:55:25 +0900

    add random pokemonId fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,25 @@ import Head from "next/head";
 import Image from "next/image";
 
 import { trpc } from "@/utils/trpc";
+import { getOptionForVote } from "@/utils/getRandonPokemon";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.hello.useQuery({ text: "Taro" });
+  const [first, second] = getOptionForVote();
 
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
+  if (!first && !second) return <div>Loading...</div>;
 
   return (
     <h1 className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between item-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800" suppressHydrationWarning>
+          {first}
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800" suppressHydrationWarning>
+          {second}
+        </div>
       </div>
     </h1>
   );
